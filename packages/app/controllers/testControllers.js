3a7c3c28657280209c5001de5355b13e204c7c80
ayushsanjdev <ayushsanjpro@gmail.com>
2024-10-19 20:18:35 +0530

    test

--- packages/app/controllers/testControllers.js
+++ packages/app/controllers/testControllers.js
@@ -25,7 +25,7 @@ const updateItem = (req, res) => {
     items[index] = { id: parseInt(req.params.id, 10), ...req.body };
     res.status(200).json(items[index]);
   } else {
-    res.status(404).json({ message: 'Item not found' });
+    res.status(404).json({ message: 'Item could not found' });
   }
 };
 
